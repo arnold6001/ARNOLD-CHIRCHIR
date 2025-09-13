df2c54715e3c8bc71b8a24bcb1a6d00c44df7470
Arnold Chirchir <arnoldkipruto193@gmail.com>
2025-09-13 17:03:16 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: '𝐉ᴜɴᴇ 𝐌ᴅ',
-  author: 'SUPREME',
-  botName: "𝐉ᴜɴᴇ 𝐌ᴅ",
-  botOwner: 'Supreme', // Your name
-  ownerNumber: '254798570132', //Set your number here without + symbol.
+  packname: 'ARNOLD JW',
+  author: 'ARNOLD CHIRCHIR',
+  botName: "ARNOLD JW",
+  botOwner: 'ARNOLD CHIRCHIR', // Your name
+  ownerNumber: '254703110780', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
